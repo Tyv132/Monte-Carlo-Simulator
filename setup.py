6f05822debcd17c0330412efd4e64e0395ce72a8
Tyler Valentine <Tyv132@protonmail.com>
2022-07-13 17:41:54 -0400

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='MonteCarlo',
+      version='1.0',
+      description="A Monte Carlo simulator containing functions to create dice, play games, and analyze the results",
+      url='https://github.com/Tyv132/Monte-Carlo-Simulator-',
+      author='Tyler Valentine',
+      license='MIT',
+      packages=['MonteCarlo'])
